242f2f7438eac222f567d3988621fb5227035191
Michailas Ornovskis <michailas@stallion.ee>
2015-11-19 14:15:29 +0200

    working repo

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix function takes matrix as an argument and CacheSolve function calculates
+## the inverse of that matrix. If inverse matrix has already been calculated and matrix value hasn't
+## changed then it will take it from cache with no calculation
+ 
 makeCacheMatrix <- function(x = matrix()) {
-
+	## store the cached value, init to NULL
+	cached_value <- NULL
+	## create matrix
+	set <- function(y) {
+	  x <<- y
+		cached_value <<- NULL
+	}
+	## get matrix value
+	get <- function() x
+	## invert matrix value and store in cache
+	set_matrix <- function(inverse) cached_value <<- inverse
+	## get inverted matrix from cache
+	get_inv_matrix <- function() cached_value
+	## load functions to R env
+	list(set = set, get = get, set_matrix = set_matrix, get_inv_matrix = get_inv_matrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates makeCacheMatrix inversed value
+## if it is possible to find that value from cache, it is
+## retrieved, if not, it is calculated and cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## try to find cached value
+	cached_value <- x$get_inv_matrix()
+	## return inverted value if it is in cache
+	if (!is.null(cached_value)) {
+	  message("cached value: ")
+		return(cached_value) 
+	}
+	## if there is no cached value, create new matrix
+	new_matrix <- x$get()
+	## invert matrix and set it in cache
+	cached_value <- solve(new_matrix)
+	x$set_matrix(cached_value)
+	## return cached value
+	return(cached_value)
 }
